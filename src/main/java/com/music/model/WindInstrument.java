3f1de0bb56596544526571ec22319ce642da875d
Aris <aris7ide@gmail.com>
2026-09-08 21:59:29 +0200

    feat: add static block and static member

--- src/main/java/com/music/model/WindInstrument.java
+++ src/main/java/com/music/model/WindInstrument.java
@@ -2,6 +2,16 @@ package com.music.model;
 
 public class WindInstrument extends Instrument {
 
+    // Bloque estatico
+    static {
+        System.out.println("--> [WindInstrument]: Clase cargada en memoria.");
+    }
+
+    // Miembro estatico de prueba
+    public static void showInfo() {
+        System.out.println("--> [WindInstrument]: Método estático invocado.");
+    }
+
     public WindInstrument(double price, String name) {
         super(price, name);
     }
